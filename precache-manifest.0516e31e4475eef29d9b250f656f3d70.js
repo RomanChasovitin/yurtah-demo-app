7f51836120099b9f5ba3314a6bd005ebf6a1b536
Сhasovitin Roman <chasovitin.r@chocolife.kz>
2019-10-24 18:23:55 +0600

    Updates

---
+++ precache-manifest.0516e31e4475eef29d9b250f656f3d70.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e913b91c15f9938761d023068721d7a",
+    "url": "/yurtah/index.html"
+  },
+  {
+    "revision": "54d9c4bf5f49dd308753",
+    "url": "/yurtah/static/css/main.1039b526.chunk.css"
+  },
+  {
+    "revision": "c693df640b08fce183e2",
+    "url": "/yurtah/static/js/2.d085ea68.chunk.js"
+  },
+  {
+    "revision": "54d9c4bf5f49dd308753",
+    "url": "/yurtah/static/js/main.76343a2d.chunk.js"
+  },
+  {
+    "revision": "711144f7175a9ffa76cc",
+    "url": "/yurtah/static/js/runtime-main.62c39b8c.js"
+  },
+  {
+    "revision": "7949563bca0e88addc6cbafc46bc80fc",
+    "url": "/yurtah/static/media/bg.7949563b.png"
+  },
+  {
+    "revision": "d12274c2982b35b9cf04da983402306e",
+    "url": "/yurtah/static/media/logo.d12274c2.svg"
+  },
+  {
+    "revision": "013b6b87b3f14d5f12f4f99879c4ea52",
+    "url": "/yurtah/static/media/oval.013b6b87.svg"
+  }
+]);
